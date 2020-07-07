1d17f622d29a07bd7c828e34f47377e20f061189
kumar0 <harishnair.p@gmail.com>
2020-07-07 13:31:14 +0100

    Add files via upload

---
+++ reduce.js
@@ -0,0 +1,200 @@
+dishes = [
+  {
+    id: 3,
+    display_order: 1,
+    title: 'Cream Of Tomato Soups',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae facilis nisi excepturi repellat possimus eum quod. Consequuntur laboriosam saepe, aliquam incidunt qui adipisci nostrum quis quas. Perferendis iusto voluptates accusamus.',
+    value: '60',
+    category: 1,
+    active: true,
+    tag_category: 'Starter',
+    tag_value: '₹ 60 ',
+    url: 'http://localhost:8000/api/product-detail/3/',
+  },
+  {
+    id: 4,
+    display_order: 2,
+    title: 'Sweet Corn Soup',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae facilis nisi excepturi repellat possimus eum quod. Consequuntur laboriosam saepe, aliquam incidunt qui adipisci nostrum quis quas. Perferendis iusto voluptates accusamus.',
+    value: '60',
+    category: 1,
+    active: true,
+    tag_category: 'Starter',
+    tag_value: '₹ 60 ',
+    url: 'http://localhost:8000/api/product-detail/4/',
+  },
+  {
+    id: 5,
+    display_order: 3,
+    title: "Hot 'N' Sour Soup",
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae facilis nisi excepturi repellat possimus eum quod. Consequuntur laboriosam saepe, aliquam incidunt qui adipisci nostrum quis quas. Perferendis iusto voluptates accusamus.',
+    value: '60',
+    category: 1,
+    active: true,
+    tag_category: 'Starter',
+    tag_value: '₹ 60 ',
+    url: 'http://localhost:8000/api/product-detail/5/',
+  },
+  {
+    id: 6,
+    display_order: 4,
+    title: 'Chilly Panner Dry',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae facilis nisi excepturi repellat possimus eum quod. Consequuntur laboriosam saepe, aliquam incidunt qui adipisci nostrum quis quas. Perferendis iusto voluptates accusamus.',
+    value: '180',
+    category: 1,
+    active: true,
+    tag_category: 'Starter',
+    tag_value: '₹ 180 ',
+    url: 'http://localhost:8000/api/product-detail/6/',
+  },
+  {
+    id: 7,
+    display_order: 5,
+    title: 'Honey Chilli Potato',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae facilis nisi excepturi repellat possimus eum quod. Consequuntur laboriosam saepe, aliquam incidunt qui adipisci nostrum quis quas. Perferendis iusto voluptates accusamus.',
+    value: '140',
+    category: 1,
+    active: true,
+    tag_category: 'Starter',
+    tag_value: '₹ 140 ',
+    url: 'http://localhost:8000/api/product-detail/7/',
+  },
+  {
+    id: 8,
+    display_order: 7,
+    title: 'Gobi Manchurian Dry',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae facilis nisi excepturi repellat possimus eum quod. Consequuntur laboriosam saepe, aliquam incidunt qui adipisci nostrum quis quas. Perferendis iusto voluptates accusamus.',
+    value: '130',
+    category: 1,
+    active: true,
+    tag_category: 'Starter',
+    tag_value: '₹ 130 ',
+    url: 'http://localhost:8000/api/product-detail/8/',
+  },
+  {
+    id: 14,
+    display_order: 10,
+    title: 'Paneer Tikka',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae facilis nisi excepturi repellat possimus eum quod. Consequuntur laboriosam saepe, aliquam incidunt qui adipisci nostrum quis quas. Perferendis iusto voluptates accusamus.',
+    value: '170',
+    category: 1,
+    active: true,
+    tag_category: 'Starter',
+    tag_value: '₹ 170 ',
+    url: 'http://localhost:8000/api/product-detail/14/',
+  },
+  {
+    id: 9,
+    display_order: 11,
+    title: 'Sambhar Idli',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae facilis nisi excepturi repellat possimus eum quod. Consequuntur laboriosam saepe, aliquam incidunt qui adipisci nostrum quis quas. Perferendis iusto voluptates accusamus.',
+    value: '40',
+    category: 4,
+    active: true,
+    tag_category: 'South Indian',
+    tag_value: '₹ 40 ',
+    url: 'http://localhost:8000/api/product-detail/9/',
+  },
+  {
+    id: 10,
+    display_order: 15,
+    title: 'Masala Dosa',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae facilis nisi excepturi repellat possimus eum quod. Consequuntur laboriosam saepe, aliquam incidunt qui adipisci nostrum quis quas. Perferendis iusto voluptates accusamus.',
+    value: '50',
+    category: 4,
+    active: true,
+    tag_category: 'South Indian',
+    tag_value: '₹ 50 ',
+    url: 'http://localhost:8000/api/product-detail/10/',
+  },
+  {
+    id: 11,
+    display_order: 20,
+    title: 'Cheese Masala Dosa',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae facilis nisi excepturi repellat possimus eum quod. Consequuntur laboriosam saepe, aliquam incidunt qui adipisci nostrum quis quas. Perferendis iusto voluptates accusamus.',
+    value: '80',
+    category: 4,
+    active: true,
+    tag_category: 'South Indian',
+    tag_value: '₹ 80 ',
+    url: 'http://localhost:8000/api/product-detail/11/',
+  },
+  {
+    id: 12,
+    display_order: 47,
+    title: 'Kaju Kari',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae facilis nisi excepturi repellat possimus eum quod. Consequuntur laboriosam saepe, aliquam incidunt qui adipisci nostrum quis quas. Perferendis iusto voluptates accusamus.',
+    value: '200',
+    category: 5,
+    active: true,
+    tag_category: 'Main Course North Indian',
+    tag_value: '₹ 200 ',
+    url: 'http://localhost:8000/api/product-detail/12/',
+  },
+  {
+    id: 2,
+    display_order: 50,
+    title: 'Butter Paneer',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae facilis nisi excepturi repellat possimus eum quod. Consequuntur laboriosam saepe, aliquam incidunt qui adipisci nostrum quis quas. Perferendis iusto voluptates accusamus.',
+    value: '200',
+    category: 2,
+    active: true,
+    tag_category: 'Main Course',
+    tag_value: '₹ 200 ',
+    url: 'http://localhost:8000/api/product-detail/2/',
+  },
+  {
+    id: 13,
+    display_order: 66,
+    title: 'Plain Rice',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae facilis nisi excepturi repellat possimus eum quod. Consequuntur laboriosam saepe, aliquam incidunt qui adipisci nostrum quis quas. Perferendis iusto voluptates accusamus.',
+    value: '80',
+    category: 2,
+    active: true,
+    tag_category: 'Main Course',
+    tag_value: '₹ 80 ',
+    url: 'http://localhost:8000/api/product-detail/13/',
+  },
+];
+const getMenuItemsbyCat = () => {
+  return Object.entries(
+    dishes.reduce((dishes, dish) => {
+      const { tag_category } = dish;
+      // console.log('tag_category', tag_category);
+      // dishes[tag_category] = dishes[tag_category]
+      //   ? console.log('True')
+      //   : console.log('False');
+
+      dishes[tag_category] = dishes[tag_category]
+        ? [...dishes[tag_category], dish]
+        : [dish];
+      //   console.log(dishes);
+      return dishes;
+    }, {})
+  );
+};
+
+dishes = getMenuItemsbyCat();
+// console.log(dishes);
+const yy = dishes.map(([item, items]) => {
+  console.log(item);
+  items.map((menu) => console.log('--->' + menu.title));
+});
+
+console.log(
+  'dishes',
+  dishes.filter((dish) => dish.id !== 13)
+);
